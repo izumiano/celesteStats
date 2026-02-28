bbb39b523247e8fdcba52726a69c670b66273db2
izumiano <izumiano@users.noreply.github.com>
2026-02-28 18:37:35 +0100

    install and configure react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import StatsPage from "./statsPage";
+import { ToastContainer } from "react-toastify";
 
 import "./App.css";
 
 export default function App() {
-	return <StatsPage />;
+	return (
+		<div>
+			<StatsPage />
+			<ToastContainer position="bottom-left" />
+		</div>
+	);
 }
