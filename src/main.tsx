2cd655f3bd6cf5fb3742992370acb7d2aa4164f6
izumiano <izumiano@users.noreply.github.com>
2026-03-06 17:06:48 +0100

    add eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+if (import.meta.env.DEV) {
+	const matches = window.location.href.match(
+		/\?console(?:=(?<config>show|hide))?/,
+	);
+	if (matches) {
+		import("eruda").then((_eruda) => {
+			const eruda = _eruda.default;
+			eruda.init();
+
+			if (matches.groups?.config === "show") {
+				eruda.show();
+			}
+		});
+	}
+}
+
 // biome-ignore lint/style/noNonNullAssertion: <root will always exist>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
