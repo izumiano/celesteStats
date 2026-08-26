31aef9a3954004ec5065faa671a20e22ce4d3892
izumiano <izumiano@users.noreply.github.com>
2026-08-26 02:12:22 +0200

    get gamabana without goldberries as well

--- src/mapsPage/chapterImage.tsx
+++ src/mapsPage/chapterImage.tsx
@@ -1,38 +1,52 @@
 import { useEffect, useState } from "react";
 import DefaultImage from "../components/defaultImage";
-import type { GoldBerriesChapter } from "./mapPage";
+import type { GamebananaCampaign, GoldBerriesChapter } from "./mapPage";
 import LoadingSpinner from "../shared/loadingSpinner";
 
 export default function ChapterImage({
-	gbChapter,
+	goldberriesChapter,
+	gamebananaCampaign,
 }: {
-	gbChapter: GoldBerriesChapter | null;
+	goldberriesChapter: GoldBerriesChapter | "pending" | null;
+	gamebananaCampaign: GamebananaCampaign | "pending" | null;
 }) {
 	const [imgUrl, setImgUrl] = useState<string | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		if (!gbChapter) {
+		if (goldberriesChapter === "pending" || gamebananaCampaign === "pending") {
+			return;
+		}
+
+		if (!goldberriesChapter && !gamebananaCampaign) {
 			setIsLoading(false);
 			return;
 		}
 
+		const gamebananaId =
+			goldberriesChapter?.campaign.gamebananaId ?? gamebananaCampaign?.id;
+
 		setIsLoading(true);
 		(async () => {
 			await (async () => {
-				let response = await fetch(
-					`https://cors-header-proxy.izumiano.workers.dev/?url=https://goldberries.net/img/map/${gbChapter.id}&scale=3`,
-				);
+				let response: Response | null = null;
+				let isMissingImage = true;
 
-				const isMissingImage =
-					response.headers.get("Cors-Proxy-Redirected") === "true";
-				if (!response.ok || isMissingImage) {
-					if (gbChapter.campaign.gamebananaId == null) {
+				if (goldberriesChapter) {
+					response = await fetch(
+						`https://cors-header-proxy.izumiano.workers.dev/?url=https://goldberries.net/img/map/${goldberriesChapter.id}&scale=3`,
+					);
+
+					isMissingImage =
+						response.headers.get("Cors-Proxy-Redirected") === "true";
+				}
+				if (!response || !response.ok || isMissingImage) {
+					if (gamebananaId == null) {
 						return;
 					}
 
 					response = await fetch(
-						`https://gamebanana.com/mods/embeddables/${gbChapter.campaign.gamebananaId}?type=large`,
+						`https://gamebanana.com/mods/embeddables/${gamebananaId}?type=large`,
 					);
 					if (!response.ok) {
 						return;
@@ -47,13 +61,17 @@ export default function ChapterImage({
 
 			setIsLoading(false);
 		})();
-	}, [gbChapter, gbChapter?.id, setIsLoading]);
+	}, [goldberriesChapter, setIsLoading, gamebananaCampaign]);
 
 	return (
 		<div className="relative">
 			<DefaultImage
 				src={imgUrl}
-				alt={gbChapter?.name}
+				alt={
+					goldberriesChapter === "pending"
+						? undefined
+						: goldberriesChapter?.name
+				}
 				width={640}
 				height={360}
 				className="chapter-image"
--- src/mapsPage/mapPage.tsx
+++ src/mapsPage/mapPage.tsx
@@ -5,11 +5,20 @@ import FullMapStats from "./fullMapStats";
 import ChapterImage from "./chapterImage";
 
 import "./mapPage.css";
+import { toast } from "react-toastify";
+import { trace } from "@izumiano/vite-logger";
 
 export interface GoldBerriesChapter {
 	id: number;
 	name: string;
+	date_added: string;
+	author_gb_name: string;
 	campaign: { id: number; name: string; url: string; gamebananaId?: number };
+}
+
+export interface GamebananaCampaign {
+	id?: number;
+	url?: string;
 }
 
 export default function MapPage() {
@@ -17,8 +26,12 @@ export default function MapPage() {
 
 	const { saveData } = useCelesteStats();
 	const [node, setNodeState] = useState<NodeStats | null>(null);
-	const [goldberriesChapter, setGoldberriesChapter] =
-		useState<GoldBerriesChapter | null>(null);
+	const [goldberriesChapters, setGoldberriesChapters] = useState<
+		GoldBerriesChapter[] | "pending" | null
+	>("pending");
+	const [gamebananaCampaign, setGamebananaCampaign] = useState<
+		GamebananaCampaign | "pending" | null
+	>("pending");
 
 	const setNode = useCallback((node: NodeStats | null) => {
 		setNodeState(node);
@@ -54,71 +67,133 @@ export default function MapPage() {
 		setNode(node);
 
 		(async () => {
-			const response = (await (
-				await fetch(
-					`https://cors-header-proxy.izumiano.workers.dev/?url=https://goldberries.net/api/map/find-by-bin-path?bin=${encodeURIComponent(sid)}.bin`,
-				)
-			).json()) as GoldBerriesChapter[];
-
-			if (response.length === 0) {
+			setGoldberriesChapters("pending");
+			const response = await fetch(
+				`https://cors-header-proxy.izumiano.workers.dev/?url=https://goldberries.net/api/map/find-by-bin-path?bin=${encodeURIComponent(sid)}.bin`,
+			);
+			if (!response.ok) {
+				setGoldberriesChapters(null);
 				return;
 			}
 
-			const chapter = response[0];
-			const gamebananaMatch = chapter.campaign.url.match(
-				/gamebanana\.com\/mods\/(\d+)/,
-			);
-			if (gamebananaMatch?.[1]) {
-				const gamebananaId = parseInt(gamebananaMatch[1]);
-				chapter.campaign.gamebananaId = Number.isNaN(gamebananaId)
-					? undefined
-					: gamebananaId;
+			const chapters = (await response.json()) as GoldBerriesChapter[];
+
+			if (chapters.length === 0) {
+				setGoldberriesChapters(null);
+				return;
 			}
 
-			setGoldberriesChapter(chapter);
+			for (const chapter of chapters) {
+				const gamebananaMatch = chapter.campaign.url.match(
+					/gamebanana\.com\/mods\/(\d+)/,
+				);
+				if (gamebananaMatch?.[1]) {
+					const gamebananaId = parseInt(gamebananaMatch[1]);
+					chapter.campaign.gamebananaId = Number.isNaN(gamebananaId)
+						? undefined
+						: gamebananaId;
+				}
+			}
+			setGoldberriesChapters(chapters);
+		})();
+
+		(async () => {
+			setGamebananaCampaign("pending");
+			const response = await fetch(
+				`https://cors-header-proxy.izumiano.workers.dev/?url=https://maddie480.ovh/celeste/gb?id=${encodeURIComponent(sid.split("/", 1)[0])}`,
+			);
+
+			if (!response.ok) {
+				setGamebananaCampaign(null);
+				return;
+			}
+
+			const html = await response.text();
+			const urlMatch = html.match(
+				/<meta property="og:url" content="https:\/\/gamebanana\.com\/mods\/(\d+)/,
+			);
+			if (!urlMatch?.[1]) {
+				toast.error("Failed getting og:url from gamebanana");
+				setGamebananaCampaign(null);
+				return;
+			}
+
+			let gamebananaId: number | undefined = parseInt(urlMatch[1]);
+			gamebananaId = Number.isNaN(gamebananaId) ? undefined : gamebananaId;
+			setGamebananaCampaign({
+				id: gamebananaId,
+				url:
+					gamebananaId != null
+						? `https://gamebanana.com/mods/${gamebananaId}`
+						: undefined,
+			});
 		})();
 	}, [saveData.levelSetStats, setNode]);
+
+	const goldberriesChapter =
+		goldberriesChapters &&
+		node &&
+		goldberriesChapters !== "pending" &&
+		gamebananaCampaign !== "pending"
+			? selectChapter(goldberriesChapters, gamebananaCampaign?.id, node.title)
+			: null;
+
+	console.log(goldberriesChapter);
 
 	return (
 		<>
 			{node && (
 				<div className="flex column map-page">
 					<main>
-						<ChapterImage gbChapter={goldberriesChapter} />
+						<ChapterImage
+							goldberriesChapter={
+								goldberriesChapters === "pending"
+									? "pending"
+									: goldberriesChapter
+							}
+							gamebananaCampaign={gamebananaCampaign}
+						/>
 						<div className="flex image-overlay">
 							<div className="flex title">
 								<div className="flex">
 									<h2>{node.title}</h2>
-									{goldberriesChapter && (
-										<div className="mapLinks">
-											<a
-												href={`https://goldberries.net/map/${goldberriesChapter.id}`}
-												target="_blank"
-												rel="noopener noreferrer"
-												className="flex mapLink"
-											>
-												<img
-													src="https://goldberries.net/favicon-32x32.png"
-													alt="goldberries link"
-													width={25}
-													height={25}
-												/>
-											</a>
-											<a
-												href={goldberriesChapter.campaign.url}
-												target="_blank"
-												rel="noopener noreferrer"
-												className="pad-left pad-right flex mapLink"
-											>
-												<img
-													src="https://images.gamebanana.com/static/img/favicon/favicon.ico"
-													alt="gamebanana link"
-													width={25}
-													height={25}
-												/>
-											</a>
-										</div>
-									)}
+									{goldberriesChapters !== "pending" &&
+										gamebananaCampaign !== "pending" &&
+										(goldberriesChapter || gamebananaCampaign?.id != null) && (
+											<div className="mapLinks">
+												{goldberriesChapter && (
+													<a
+														href={`https://goldberries.net/map/${goldberriesChapter.id}`}
+														target="_blank"
+														rel="noopener noreferrer"
+														className="flex mapLink"
+													>
+														<img
+															src="https://goldberries.net/favicon-32x32.png"
+															alt="goldberries link"
+															width={25}
+															height={25}
+														/>
+													</a>
+												)}
+												<a
+													href={
+														goldberriesChapter?.campaign.url ??
+														gamebananaCampaign?.url
+													}
+													target="_blank"
+													rel="noopener noreferrer"
+													className="pad-left pad-right flex mapLink"
+												>
+													<img
+														src="https://images.gamebanana.com/static/img/favicon/favicon.ico"
+														alt="gamebanana link"
+														width={25}
+														height={25}
+													/>
+												</a>
+											</div>
+										)}
 								</div>
 							</div>
 						</div>
@@ -146,4 +221,56 @@ export default function MapPage() {
 			)}
 		</>
 	);
+}
+
+function selectChapter(
+	chapters: GoldBerriesChapter[],
+	gamebananaId: number | undefined,
+	mapName: string,
+) {
+	if (gamebananaId != null) {
+		trace("Attempting to match gamebanana id");
+		const gamebananaMatches = chapters.filter(
+			(chapter) => chapter.campaign.gamebananaId === gamebananaId,
+		);
+		if (gamebananaMatches.length > 0) {
+			chapters = gamebananaMatches;
+		} else {
+			toast.warn(
+				`Goldberries did not respond with any chapter matching gamebanana id '${gamebananaId}'`,
+			);
+		}
+	}
+	if (chapters.length === 1) {
+		trace("Only 1 response or matched gamebanana id");
+		return chapters[0];
+	}
+
+	const nameMatches = chapters.filter((chapter) => chapter.name === mapName);
+	if (nameMatches.length > 0) {
+		chapters = nameMatches;
+	}
+	if (chapters.length === 1) {
+		trace("Matched name");
+		return chapters[0];
+	}
+
+	const withAuthor = chapters.filter((chapter) => chapter.author_gb_name);
+	if (withAuthor.length > 0) {
+		chapters = withAuthor;
+	}
+	if (chapters.length === 1) {
+		trace("Matched having an author");
+		return chapters[0];
+	}
+
+	chapters.sort((chapterA, chapterB) => {
+		return (
+			new Date(chapterA.date_added).getTime() -
+			new Date(chapterB.date_added).getTime()
+		);
+	});
+
+	trace("First by date");
+	return chapters[0];
 }
